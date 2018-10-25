800578cf2225516a98b06f173dca0d97b9b9b440
ulestrange <lestrange.una@itsligo.ie>
2018-10-25 13:52:56 +0100

    added some layout stuff

---
+++ Sample/Sample/Pages/form1.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sample.Pages
+{
+    public class Form1Model : PageModel
+    {
+        [BindProperty]
+        public bool HasVisitedSligo { get; set; }
+
+        [BindProperty]
+        public bool[] HasVisitedCounty { get; set; } =
+            {false, false, false, false, false};
+
+        public string[] CountyList { get; set; }
+        = { "Sligo", "Leitrim", "Mayo", "Roscommon", "Galway" };
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
